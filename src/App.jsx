a43187174f8d7031b58a13466cc645d26561d547
atharv1205 <atharvadudhe125@gmail.com>
2025-02-19 13:53:58 +0530

    completed assignment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hi, I am Atharva</h1>
+      <p>Email: atharva.dudhe.s73@kalvium.community</p>
+    </div>
+  );
+}
+
+export default App
